deaa07e2acff2847acdc0015e054a4009ea83f32
Cyril <c.concas.ext@axione.fr>
2024-04-22 11:21:38 +0200

    First commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const name = "David"; // ✅ Good!  
+const lastname = "Bruno"; // ❌ the keyword to declare the variable is missing 
+
+  
+  let biography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; // ❌ the variable name is not explicit. (this is David's biography)
+
+
+  const avatar = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+
+  let city = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+
+  let followers = 109; // ❌ the keyword to declare the variable is missing 
+  following = 200; // ❌ the keyword to declare the variable is missing 
+
+// 🏁 Exercise 02 - David is following one more account increment the total of following account
